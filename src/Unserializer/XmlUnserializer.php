4f9bc90734369d448229f82878b5c8dc0aa5ba42
João Paulo Cercal <jpcercal@gmail.com>
2015-06-25 15:55:34 -0300

    Added the class XmlUnserializer that throw a Exception when it is called.

---
+++ src/Unserializer/XmlUnserializer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Humantech\Zoho\Recruit\Api\Unserializer;
+
+class XmlUnserializer implements UnserializerInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function unserialize($data)
+    {
+        throw new \LogicException('The method XmlUnserializer::unserialize() is not implemented.');
+    }
+}
