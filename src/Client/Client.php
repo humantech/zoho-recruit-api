d08150ab7af9c9b6e2ced11f965319841f2aaa2c
João Paulo Cercal <jpcercal@gmail.com>
2015-06-25 17:24:16 -0300

    Added the protected method callApi()

--- src/Client/Client.php
+++ src/Client/Client.php
@@ -2,6 +2,8 @@
 
 namespace Humantech\Zoho\Recruit\Api\Client;
 
+use GuzzleHttp\Psr7\Response;
+
 class Client extends AbstractClient implements ClientInterface
 {
     const API_BASE_URL = 'https://recruit.zoho.com/recruit/private/%s/%s/%s?authtoken=%s';
@@ -25,6 +27,36 @@ class Client extends AbstractClient implements ClientInterface
     public function __construct($authToken)
     {
         $this->authToken = $authToken;
+    }
+
+    /**
+     * @param  string $httpMethod
+     * @param  string $module
+     * @param  string $method
+     * @param  string $responseFormat
+     * @param  array  $requestParameters
+     *
+     * @return Response
+     */
+    protected function callApi($httpMethod, $module, $method, $responseFormat, array $requestParameters)
+    {
+        if (!isset($requestParameters['scope'])) {
+            $requestParameters['scope'] = self::API_DEFAULT_SCOPE;
+        }
+
+        if (!isset($requestParameters['version'])) {
+            $requestParameters['version'] = self::API_DEFAULT_VERSION;
+        }
+
+        $uri = sprintf(
+            self::API_BASE_URL,
+            $responseFormat,
+            $module,
+            $method,
+            $this->getAuthToken()
+        );
+
+        return $this->sendRequest($httpMethod, $uri);
     }
 
     /**
