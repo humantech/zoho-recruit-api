9bed3e1e755e596573726974ac8bdcee6563392f
João Paulo Cercal <jpcercal@gmail.com>
2015-06-25 16:37:49 -0300

    Implemented the method generateAuthToken that get a new token from Zoho Recruit API given a username and password.

--- src/Client/AuthenticationClient.php
+++ src/Client/AuthenticationClient.php
@@ -5,4 +5,34 @@ namespace Humantech\Zoho\Recruit\Api\Client;
 class AuthenticationClient extends AbstractClient
 {
     const API_AUTH_URL = 'https://accounts.zoho.com/apiauthtoken/nb/create?SCOPE=%s&EMAIL_ID=%s&PASSWORD=%s';
+
+    /**
+     * @param  string $username
+     * @param  string $password
+     *
+     * @return string
+     *
+     * @throws AuthenticationClientException
+     */
+    public function generateAuthToken($username, $password)
+    {
+        $response = $this->sendRequest('GET', sprintf(
+            self::API_AUTH_URL,
+            'ZohoRecruit/recruitapi',
+            urlencode($username),
+            urlencode($password)
+        ));
+
+        $content = $response->getBody()->getContents();
+
+        if (strpos($content, 'RESULT=TRUE') === false) {
+            preg_match('/CAUSE\=(\w*)/i', $content, $matches);
+
+            throw new AuthenticationClientException($matches[1]);
+        }
+
+        preg_match('/AUTHTOKEN\=(\S*)/i', $content, $matches);
+
+        return $matches[1];
+    }
 }
