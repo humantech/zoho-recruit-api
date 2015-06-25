a321167b408fef137edba0e1453c58662a9be294
João Paulo Cercal <jpcercal@gmail.com>
2015-06-25 16:39:23 -0300

    Implemented a interface to store and retrieve the URI in the class HttpApiException.

--- src/Client/HttpApiException.php
+++ src/Client/HttpApiException.php
@@ -4,4 +4,29 @@ namespace Humantech\Zoho\Recruit\Api\Client;
 
 class HttpApiException extends \RuntimeException
 {
+    /**
+     * @var string
+     */
+    protected $uri;
+
+    /**
+     * @param string     $message
+     * @param int        $code
+     * @param string     $uri
+     * @param \Exception $previous
+     */
+    public function __construct($message, $code, $uri, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->uri = $uri;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
 }
