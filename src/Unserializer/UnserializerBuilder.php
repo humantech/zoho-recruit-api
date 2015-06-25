1cd384c493efbd26bc6cd6198d49ea56b372594e
João Paulo Cercal <jpcercal@gmail.com>
2015-06-25 15:56:29 -0300

    Implemented the UnserializerBuilder class that unserialize data given a response format.

---
+++ src/Unserializer/UnserializerBuilder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Humantech\Zoho\Recruit\Api\Unserializer;
+
+class UnserializerBuilder implements UnserializerInterface
+{
+    /**
+     * @var UnserializerInterface
+     */
+    private $unserializer;
+
+    /**
+     * @param string $responseFormat
+     */
+    private function __construct($responseFormat)
+    {
+        $this->unserializer = $this->getUnserializerByResponseFormat($responseFormat);
+    }
+
+    /**
+     * @param  string $responseFormat
+     *
+     * @return UnserializerInterface
+     *
+     * @throws \InvalidArgumentException
+     */
+    private function getUnserializerByResponseFormat($responseFormat)
+    {
+        switch (strtolower($responseFormat)) {
+            case 'json':
+                return new JsonUnserializer();
+                break;
+
+            case 'xml':
+                return new XmlUnserializer();
+                break;
+        }
+
+        throw new \InvalidArgumentException('The format of the response is invalid');
+    }
+
+    /**
+     * @param string $responseFormat
+     *
+     * @return UnserializerBuilder
+     */
+    public static function create($responseFormat)
+    {
+        return new static($responseFormat);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function unserialize($data)
+    {
+        return $this->unserializer->unserialize($data);
+    }
+}
