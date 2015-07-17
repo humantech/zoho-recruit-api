d7be20375f50500d7a3c0abf4c61e3a176f563dc
João Paulo Cercal <jpcercal@gmail.com>
2015-07-17 14:46:39 -0300

    Added the tests to ResponseFormatter class.

--- src/Tests/Formatter/ResponseFormatterTest.php
+++ src/Tests/Formatter/ResponseFormatterTest.php
@@ -14,6 +14,7 @@ class ResponseFormatterTest extends TestCase
             array('getRecordById'),
             array('getNoteTypes'),
             array('getRelatedRecords'),
+            array('getAssociatedJobOpenings'),
             array('getAssociatedCandidates'),
             array('getSearchRecords'),
         );
