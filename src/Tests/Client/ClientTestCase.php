dba83fde39347c7672b05679e95ea8bc12ff3f9b
João Paulo Cercal <jpcercal@gmail.com>
2015-07-17 14:45:45 -0300

    Added the base Client Test Case to test Client classes.

--- src/Tests/Client/ClientTestCase.php
+++ src/Tests/Client/ClientTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Humantech\Zoho\Recruit\Api\Tests\Client;
 
+use Humantech\Zoho\Recruit\Api\Client\Client;
 use Humantech\Zoho\Recruit\Api\Tests\TestCase;
 
 class ClientTestCase extends TestCase
@@ -15,5 +16,29 @@ class ClientTestCase extends TestCase
     public function getResourceFakeResponseByName($filename)
     {
         return file_get_contents(realpath(dirname(__FILE__) . '/Resources/FakeResponse/' . $filename));
+    }
+
+    /**
+     * @param  string $token
+     *
+     * @return Client
+     */
+    public function getClientOriginalInstance($token = 'fake_token')
+    {
+        return new Client($token);
+    }
+
+    /**
+     * @param  array $methods
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    public function getClientMock(array $methods)
+    {
+        return $this->getMockBuilder('\\Humantech\\Zoho\\Recruit\\Api\\Client\\Client')
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock()
+        ;
     }
 }
