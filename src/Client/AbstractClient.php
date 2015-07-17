45acab45433ede9a3ad60d9216fada33112c3705
João Paulo Cercal <jpcercal@gmail.com>
2015-07-17 14:25:26 -0300

    Added the method sendFile using the curl extension natively.

--- src/Client/AbstractClient.php
+++ src/Client/AbstractClient.php
@@ -32,5 +32,24 @@ abstract class AbstractClient
 
         return $client->send($request, $clientOptions);
     }
+
+    /**
+     * @param  string $uri
+     * @param  array  $postFields
+     *
+     * @return string|bool
+     */
+    protected function sendFile($uri, array $postFields)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $uri);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+
+        return curl_exec($ch);
     }
 }
